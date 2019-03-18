9c2507c1a531379a5ed87ade12c700c43c4f0f8e
huangqiang <huang50179@163.com>
2019-03-18 16:06:15 +0800

    update:修改druid数据库连接池配置

--- src/main/java/com/hq/common/rest/Result.java
+++ src/main/java/com/hq/common/rest/Result.java
@@ -1,5 +1,6 @@
 package com.hq.common.rest;
 
+
 import lombok.Getter;
 import lombok.Setter;
 
@@ -14,26 +15,30 @@ public class Result<T>
     private Integer code;
     private String msg;
     private T data;
+    private long timestamp;
 
     public Result()
     {
+        this.timestamp = System.currentTimeMillis() / 1000;
     }
-
     public Result(Integer code)
     {
         this.code = code;
+        this.timestamp = System.currentTimeMillis() / 1000;
     }
 
     public Result(Integer code, String msg)
     {
         this.code = code;
         this.msg = msg;
+        this.timestamp = System.currentTimeMillis() / 1000;
     }
 
     public Result(Integer code, T data)
     {
         this.code = code;
         this.data = data;
+        this.timestamp = System.currentTimeMillis() / 1000;
     }
 
 }
